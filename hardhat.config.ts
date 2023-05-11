500fd61d0c191b5ae1ede1763fc76747751b19f6
AntonD3 <antonv.dyadyuk@gmail.com>
2023-05-12 01:09:49 +0200

    Refactor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,24 +2,29 @@ import '@matterlabs/hardhat-zksync-solc'
 
 export default {
     networks: {
+        zkSyncLocalhost: {
+            url: "http://localhost:3050",
+            ethNetwork: "http://localhost:8545",
+            zksync: true,
+        },
         zkSyncTestnet: {
-            url: "https://testnet.era.zksync.dev",
+            url: 'https://testnet.era.zksync.dev',
             ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
             zksync: true,
         },
-    },
-    solidity: {
-        version: '0.5.16',
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 999999,
-            },
+        zkSyncMainnet: {
+            url: 'https://mainnet.era.zksync.io',
+            ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            zksync: true,
         },
     },
+    defaultNetwork: 'zkSyncLocalhost',
+    solidity: {
+        version: '0.5.16',
+    },
     zksolc: {
-        version: "1.3.10",
-        compilerSource: "binary",
+        version: '1.3.10',
+        compilerSource: 'binary',
         settings: {},
     },
 }
