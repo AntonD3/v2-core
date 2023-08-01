2125dda4928d483b5263646109d17b22dde22384
AntonD3 <antonv.dyadyuk@gmail.com>
2023-08-01 12:24:23 +0200

    Fix formatting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,30 @@
 import '@matterlabs/hardhat-zksync-solc'
 
 export default {
-    networks: {
-        zkSyncLocalhost: {
-            url: 'http://localhost:3050',
-            ethNetwork: 'http://localhost:8545',
-            zksync: true,
-        },
-        zkSyncTestnet: {
-            url: 'https://testnet.era.zksync.dev',
-            ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-            zksync: true,
-        },
-        zkSyncMainnet: {
-            url: 'https://mainnet.era.zksync.io',
-            ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-            zksync: true,
-        },
+  networks: {
+    zkSyncLocalhost: {
+      url: 'http://localhost:3050',
+      ethNetwork: 'http://localhost:8545',
+      zksync: true,
     },
-    defaultNetwork: 'zkSyncLocalhost',
-    solidity: {
-        version: '0.5.16',
+    zkSyncTestnet: {
+      url: 'https://testnet.era.zksync.dev',
+      ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
     },
-    zksolc: {
-        version: '1.3.13',
-        compilerSource: 'binary',
-        settings: {},
+    zkSyncMainnet: {
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
     },
+  },
+  defaultNetwork: 'zkSyncLocalhost',
+  solidity: {
+    version: '0.5.16',
+  },
+  zksolc: {
+    version: '1.3.13',
+    compilerSource: 'binary',
+    settings: {},
+  },
 }
